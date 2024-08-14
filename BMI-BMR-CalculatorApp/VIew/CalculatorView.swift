3351e49a13f71a55e5d23dbd2ea98395111323f6
Danil Anikin <mart4r.clearlist@gmail.com>
2024-08-15 00:58:29 +1000

    Create CalculatorView

--- BMI-BMR-CalculatorApp/VIew/CalculatorView.swift
+++ BMI-BMR-CalculatorApp/VIew/CalculatorView.swift
@@ -8,8 +8,164 @@
 import SwiftUI
 
 struct CalculatorView: View {
+  @State var height: Double = 150
+  @State var age = 18
+  @State var weight = 50
+  
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+      VStack(spacing: 25) {
+        HStack {
+          // MARK: Fe/male
+          Button {
+            
+          } label: {
+            VStack {
+              Image("male")
+                .resizable()
+                .frame(width: 96, height: 96)
+              Text("Male")
+                .foregroundStyle(.gray)
+                .font(.system(size: 20))
+            }
+          }
+          .padding(24)
+          .background(Color("PLATECOLOR"))
+          .clipShape(RoundedRectangle(cornerRadius: 12))
+          Spacer()
+          Button {
+            
+          } label: {
+            VStack {
+              Image("female")
+                .resizable()
+                .frame(width: 96, height: 96)
+              Text("Female")
+                .foregroundStyle(.gray)
+                .font(.system(size: 20))
+            }
+          }
+          .padding(24)
+          .background(Color("PLATECOLOR"))
+          .clipShape(RoundedRectangle(cornerRadius: 12))
+        }
+        .padding(.horizontal, 25)
+        
+        
+        // MARK: Height
+        VStack {
+          Text("Height")
+            .foregroundStyle(.gray)
+            .font(.system(size: 20))
+          HStack(alignment: .bottom, spacing: 0) {
+            Text(String(Int(height)))
+              .foregroundStyle(.white)
+              .font(.system(size: 40, weight: .bold))
+            Text("cm")
+              .foregroundStyle(.white)
+              .font(.system(size: 20, weight: .medium))
+              .padding(.bottom, 5)
+          }
+          Slider(value: $height, in: 1...300, step: 1) { editing in
+            
+          }
+          .accentColor(Color("MAIN"))
+        }
+        .padding(24)
+        .frame(maxWidth: .infinity)
+        .background(Color("PLATECOLOR"))
+        .clipShape(RoundedRectangle(cornerRadius: 12))
+        .padding(.horizontal, 25)
+        
+        // MARK: Weight and Age
+        HStack {
+          VStack {
+            Text("Weight")
+              .foregroundStyle(.gray)
+            Text(String(weight))
+              .font(.system(size: 60, weight: .bold))
+              .foregroundStyle(.white)
+              .padding(.bottom, 10)
+            HStack (spacing: 40) {
+              Button {
+                
+              } label: {
+                Image(systemName: "minus")
+                  .resizable()
+                  .frame(width: 15, height: 3)
+                  .padding(16)
+                  .foregroundStyle(.white)
+                  .background(Color("GRAY"))
+                  .clipShape(Circle())
+              }
+              Button {
+                
+              } label: {
+                Image(systemName: "plus")
+                  .resizable()
+                  .frame(width: 15, height: 15)
+                  .padding(10)
+                  .foregroundStyle(.white)
+                  .background(Color("GRAY"))
+                  .clipShape(Circle())
+              }
+            }
+          }
+          .padding(24)
+          .background(Color("PLATECOLOR"))
+          .clipShape(RoundedRectangle(cornerRadius: 12))
+          
+          Spacer()
+          
+          VStack {
+            Text("Age")
+              .foregroundStyle(.gray)
+            Text(String(age))
+              .font(.system(size: 60, weight: .bold))
+              .foregroundStyle(.white)
+              .padding(.bottom, 10)
+            HStack (spacing: 40) {
+              Button {
+                
+              } label: {
+                Image(systemName: "minus")
+                  .resizable()
+                  .frame(width: 15, height: 3)
+                  .padding(16)
+                  .foregroundStyle(.white)
+                  .background(Color("GRAY"))
+                  .clipShape(Circle())
+              }
+              Button {
+                
+              } label: {
+                Image(systemName: "plus")
+                  .resizable()
+                  .frame(width: 15, height: 15)
+                  .padding(10)
+                  .foregroundStyle(.white)
+                  .background(Color("GRAY"))
+                  .clipShape(Circle())
+              }
+            }
+          }
+          .padding(24)
+          .background(Color("PLATECOLOR"))
+          .clipShape(RoundedRectangle(cornerRadius: 12))
+        }
+        .padding(.horizontal, 25)
+        .padding(.bottom, 30)
+        
+        Button {
+          
+        } label: {
+          Text("Calculate")
+            .font(.system(size: 32, weight: .black))
+            .foregroundStyle(.white)
+            .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .bottom)
+            .background(Color("MAIN"))
+
+        }
+      }
     }
 }
 
--- BMI-BMR-CalculatorApp/VIew/ContentView.swift
+++ BMI-BMR-CalculatorApp/VIew/ContentView.swift
@@ -9,7 +9,20 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+      VStack {
+        HStack {
+          Text("BMI/BMR Calculator")
+            .foregroundStyle(.white)
+            .font(.system(size: 20, weight: .black))
+            .frame(maxWidth: .infinity)
+            .padding(.bottom, 10)
+            .background(Color("UPBGCOLOR"))
+        }
+        Spacer()
         CalculatorView()
+      }
+      .frame(maxWidth: .infinity, maxHeight: .infinity)
+      .background(Color("BGCOLOR"))
     }
 }
 
