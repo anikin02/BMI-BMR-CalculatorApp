d45cd6450ba0dea39409fde087a8e90d786e1363
Danil Anikin <mart4r.clearlist@gmail.com>
2024-08-14 22:29:38 +1000

    Initial Commit

---
+++ BMI-BMR-CalculatorApp/BMI_BMR_CalculatorAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BMI_BMR_CalculatorAppApp.swift
+//  BMI-BMR-CalculatorApp
+//
+//  Created by Данил Аникин on 14.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct BMI_BMR_CalculatorAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BMI-BMR-CalculatorApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BMI-BMR-CalculatorApp
+//
+//  Created by Данил Аникин on 14.08.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
